a166a333d0f057c10e4f70fb622999a90021bd4a
Gabriel Henrique <ev04hs@gmail.com>
2023-04-02 14:10:32 -0300

    remove pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -59,6 +59,24 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position){
+
+        if(!positionExists(position)){
+
+            throw new BoardExeption("Position not on the board !");
+        }
+        if(piece(position)==null){
+
+            return null;
+        }
+
+        Piece aux = piece(position);
+        aux.position=null;
+        pieces[position.getRow()][position.getColumn()]=null;
+
+        return aux;
+    }
+
     //auxiliar method
     private boolean positionExists(int row, int column){
 
@@ -80,6 +98,8 @@ public class Board {
         }
         return piece(position) != null;
     }
+
+
 
 
 }

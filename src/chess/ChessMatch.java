2ccf2e383da313fa7b1e7040d997db54cc514475
Gabriel Henrique <ev04hs@gmail.com>
2023-04-05 20:47:48 -0300

    Pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 import java.util.ArrayList;
@@ -257,12 +258,18 @@ public class ChessMatch {
     private void initialSetup(){
 
         placeNewPiece('e',1, new King(board, Color.WHITE) );
-        placeNewPiece('d',1, new Rook(board, Color.WHITE) );
-        placeNewPiece('h',7, new Rook(board, Color.WHITE) );
+        placeNewPiece('a',1, new Rook(board, Color.WHITE) );
+        placeNewPiece('h',1, new Rook(board, Color.WHITE) );
+        for(int i=0;i<8;i++){
+            placeNewPiece((char) ('a'+i),2,new Pawn(board, Color.WHITE));
+        }
 
-        placeNewPiece('a',8, new King(board, Color.BLACK) );
-        placeNewPiece('b',8, new Rook(board, Color.BLACK) );
-
+        placeNewPiece('d',8, new King(board, Color.BLACK) );
+        placeNewPiece('a',8, new Rook(board, Color.BLACK) );
+        placeNewPiece('h',8, new Rook(board, Color.BLACK) );
+        for(int i=0;i<8;i++){
+            placeNewPiece((char) ('a'+i),7,new Pawn(board, Color.BLACK));
+        }
 
     }
 }

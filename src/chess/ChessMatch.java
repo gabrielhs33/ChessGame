6a5bdfef27ccdc038a1333284991cdf755966a12
Gabriel Henrique <ev04hs@gmail.com>
2023-04-05 21:23:46 -0300

    Queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,10 +6,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
-import chess.pieces.Bishop;
-import chess.pieces.King;
-import chess.pieces.Pawn;
-import chess.pieces.Rook;
+import chess.pieces.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -259,6 +256,7 @@ public class ChessMatch {
     private void initialSetup(){
 
         placeNewPiece('e',1, new King(board, Color.WHITE) );
+        placeNewPiece('d',1, new Queen(board, Color.WHITE) );
         placeNewPiece('a',1, new Rook(board, Color.WHITE) );
         placeNewPiece('h',1, new Rook(board, Color.WHITE) );
         placeNewPiece('c',1, new Bishop(board, Color.WHITE) );
@@ -268,6 +266,7 @@ public class ChessMatch {
         }
 
         placeNewPiece('e',8, new King(board, Color.BLACK) );
+        placeNewPiece('d',8, new Queen(board, Color.WHITE) );
         placeNewPiece('a',8, new Rook(board, Color.BLACK) );
         placeNewPiece('h',8, new Rook(board, Color.BLACK) );
         placeNewPiece('c',8, new Bishop(board, Color.BLACK) );
